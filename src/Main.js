300d193a42745deaf3ea89d4e0fa48fe885633df
NA <googmlgml@gmail.com>
2023-02-19 21:51:51 +0900

    React-Redux

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { createStore } from "redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
+
+const reducer = (state, action) => {
+  if (action.type === "up") {
+    return { ...state, value: state.value + action.step };
+  }
+  return state;
+};
+
+const initialState = { value: 0 };
+const store = createStore(reducer, initialState);
+const Counter = () => {
+  const count = useSelector((state) => state.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch({ type: "up", step: 2 });
+        }}
+      >
+        +
+      </button>
+      {count}
+    </div>
+  );
+};
 
 const Main = () => {
-  return <div>Main page입니다.</div>;
+  return (
+    <>
+      <Provider store={store}>
+        <Counter></Counter>
+      </Provider>
+    </>
+  );
 };
 
 export default Main;
